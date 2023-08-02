8c9bd1c3abe36d93840ed9bcbc21ff8ad189c72d
nossiro <orissongomes@gmail.com>
2023-08-02 11:30:23 -0300

    controlando pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoTelaJogo(){
 	altura = window.innerHeight
@@ -15,6 +16,14 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+        
+        if (vidas > 3) {
+            alert('Interromper o jogo (game over)')
+        }
+        else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
     }
 
 
@@ -34,6 +43,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
