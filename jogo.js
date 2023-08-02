78c488ba5ccb26e677a6d886b92a0452863efe0e
nossiro <orissongomes@gmail.com>
2023-08-02 10:11:39 -0300

    criando posicoes randomicas

--- jogo.js
+++ jogo.js
@@ -4,9 +4,29 @@ var largura = 0
 function ajustaTamanhoTelaJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
-    
-    console.log(altura, largura)
+
+    console.log(largura, altura)
 }
 
 ajustaTamanhoTelaJogo()
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoX = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criando elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
 
