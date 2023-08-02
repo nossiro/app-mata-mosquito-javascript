74eefb0b28ea9a282a6d4a6efa68a8b1274f5822
nossiro <orissongomes@gmail.com>
2023-08-02 14:29:56 -0300

    adicionando cronometro

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 3
 
 function ajustaTamanhoTelaJogo(){
 	altura = window.innerHeight
@@ -10,6 +11,21 @@ function ajustaTamanhoTelaJogo(){
 }
 
 ajustaTamanhoTelaJogo()
+
+var cronometro = setInterval(function(){
+
+    tempo -=1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('Vitória')
+    }
+    else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+
+}, 1000)
 
 function posicaoRandomica() {
 
