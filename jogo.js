274731a67ec6d81aa7e626c083d9ce629f962c18
nossiro <orissongomes@gmail.com>
2023-08-02 09:43:23 -0300

    Definindo dimensões da tela do jogo

---
+++ jogo.js
@@ -0,0 +1,12 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoTelaJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+    
+    console.log(altura, largura)
+}
+
+ajustaTamanhoTelaJogo()
+
